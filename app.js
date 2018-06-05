523ed5bee52d5ae5a51061e6889f79d06ce1fac6
adnanrahic <ado.raha198@gmail.com>
2018-06-05 18:07:40 +0200

    testing prod

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res, next) => {
-  res.status(200).send('Hello World!');
-});
-
-app.get('/env', (req, res, next) => {
-  res.status(200).send(process.env.NODE_ENV);
-});
-
-// 1. Regular Node.js server
-// app.listen(3000);
-
-// 2. Packaging the server up to be deployed on Lambda
-const serverless = require('serverless-http');
-module.exports.server = serverless(app);
+const express = require('express')
+const sls = require('serverless-http')
+const app = express()
+app.get('/', async (req, res, next) => {
+  res.status(200).send('Hello World!')
+})
+module.exports.server = sls(app)
