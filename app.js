4926a0030bc9e26285f4b46358460b4041b43b8b
adnanrahic <ado.raha198@gmail.com>
2018-11-20 10:44:03 +0100

    testing

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const sls = require('serverless-http')
 const app = express()
-app.get('/', async (req, res, next) => {
-  res.status(200).send('Hello World!')
+// throw Error('Well this broke.')
+app.get('/', (req, res, next) => {
+  res.status(200).send('API works.')
 })
+app.get('/error', (req, res, next) => {
+  const err = new Error('Not found.')
+  res.status(404).send(err.message)
+})
+app.get('/internal_error', (req, res, next) => {
+  const err = new Error('This died...')
+  throw err
+})
+// app.use((err, req, res, next) => {
+//   throw err
+// })
 module.exports.server = sls(app)
